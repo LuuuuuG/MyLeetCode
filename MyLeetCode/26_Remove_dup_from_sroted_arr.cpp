82dfef4b77be673e0c1b7b3ade84827a7be8dce9
Gang <776881718@qq.com>
2018-07-21 15:56:37 +0800

    27. Remove Element.

--- MyLeetCode/26_Remove_dup_from_sroted_arr.cpp
+++ MyLeetCode/26_Remove_dup_from_sroted_arr.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 /*
-https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+26: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+27: https://leetcode.com/problems/remove-element/description/
 ATTENTION!!!¡¾It doesn't matter what values are set beyond the returned length.¡¿@_@
 */
 
@@ -17,14 +18,22 @@ int removeDuplicates(vector<int>& nums)
 
 }
 
+int removeElement(vector<int>& nums, int val) {
+	int i = 0;
+	for (int n : nums)
+	if (n != val)
+		nums[i++] = n;
+	return i;
+}
 
 
-int main()
+int main_26_27()
 {
-	vector<int> v = { 1, 1, 1, 2, 3, 4, 4, 5, 5, 8 };
+	vector<int> v = { 1, 2, 2, 3, 4, 4, 5, 5, 8 };
 
-	int res = removeDuplicates(v);
-	
+	//int res = removeDuplicates(v);
+	int res = removeElement(v, 2);
+
 	cout << "length = " << res << endl;
 	for (int i = 0; i < res;++i)
 		cout << " " << v[i];
