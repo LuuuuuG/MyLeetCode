a6c7ead4b93d97d5d89e5b6341270cd7cd436129
Gang <776881718@qq.com>
2019-01-11 22:49:26 +0800

    78_Subsets. 90_Subsets II.

---
+++ Source/78_Subsets.cpp
@@ -0,0 +1,60 @@
+#include <00_util.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+	[3],
+	[1],
+	[2],
+	[1,2,3],
+	[1,3],
+	[2,3],
+	[1,2],
+	[]
+]
+
+78. Subsets: https://leetcode.com/problems/subsets/description/
+*/
+
+class Solution {
+public:
+	vector<vector<int>> subsets(vector<int>& nums) {
+		vector<vector<int>> subs;
+		vector<int> sub;
+		subsets(nums, 0, sub, subs);
+		return subs;
+	}
+private:
+	void subsets(vector<int>& nums, int start, vector<int>& sub, vector<vector<int>>& subs) {
+		subs.push_back(sub);
+		for (int i = start; i < nums.size(); i++)
+		{
+			sub.push_back(nums[i]);
+			subsets(nums, i + 1, sub, subs);
+			sub.pop_back();
+		}
+	}
+};
+
+
+int main_78()
+{
+	Solution s;
+	vector<int> v{ 1, 2, 3 }; 
+	vector<vector<int> > res = s.subsets(v);
+
+	//Vector2DPrint(res);
+
+	system("pause");
+	return 0;
+}
---
+++ Source/90_Subsets II.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: [1,2,2]
+Output:
+[
+	[2],
+	[1],
+	[1,2,2],
+	[2,2],
+	[1,2],
+	[]
+]
+
+90. Subsets II: https://leetcode.com/problems/subsets-ii/description/
+*/
+
+void subsets(vector<int>& nums, int start, vector<int>& sub, vector<vector<int> >& res){
+	res.push_back(sub);
+	for (int i = start; i < nums.size(); ++i)
+	{
+		if (i > start && nums[i] == nums[i - 1]) continue;
+		sub.push_back(nums[i]);
+		subsets(nums, i + 1, sub, res);
+		sub.pop_back();
+	}
+}
+
+vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+	vector<vector<int> > res;
+	vector<int> sub;
+	sort(nums.begin(), nums.end());
+	subsets(nums, 0, sub, res);
+	return res;
+}
+
+int main_90()
+{
+	vector<int> v{ 1, 2, 2 };
+	vector<vector<int> > res = subsetsWithDup(v);
+	for (auto v : res)
+	{
+		for (auto i : v)
+			cout << i << " ";
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
