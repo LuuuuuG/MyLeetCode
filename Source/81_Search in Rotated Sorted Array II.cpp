39158708f1e19ecf54a7f60997cd1a845853c787
Gang <776881718@qq.com>
2019-01-18 17:02:49 +0800

    ...

--- Source/81_Search in Rotated Sorted Array II.cpp
+++ Source/81_Search in Rotated Sorted Array II.cpp
@@ -29,8 +29,10 @@ Would this affect the run-time complexity? How and why?
 bool search(vector<int>& nums, int target) {
 
 	int start = 0, end = nums.size();
+	sort(nums.begin(), nums.end());
 	while (start < end)
 	{
+		
 		int mid = start + (end - start) / 2;
 		int num = 0;
 		if ((nums[mid] < nums[0]) == (target < nums[0]))
@@ -47,12 +49,49 @@ bool search(vector<int>& nums, int target) {
 	}
 	return false;
 }
+/*
+1) everytime check if targe == nums[mid], if so, we find it.
 
-int main_NG()
+2) otherwise, we check if the first half is in order (i.e. nums[left]<=nums[mid])
+  and if so, go to step 3), otherwise, the second half is in order,   go to step 4)
+
+3) check if target in the range of [left, mid-1] (i.e. nums[left]<=target < nums[mid]), 
+if so, do search in the first half, i.e. right = mid-1; otherwise, search in the second half left = mid+1;
+
+4)  check if target in the range of [mid+1, right] (i.e. nums[mid]<target <= nums[right]), 
+if so, do search in the second half, i.e. left = mid+1; otherwise search in the first half right = mid-1;
+*/
+bool search2(vector<int>& nums, int target) {
+	int left = 0, right = nums.size() - 1, mid;
+
+	while (left <= right)
+	{
+		mid = (left + right) >> 1;
+		if (nums[mid] == target) return true;
+
+		// the only difference from the first one, trickly case, just updat left and right
+		if ((nums[left] == nums[mid]) && (nums[right] == nums[mid])) { ++left; --right; }
+		
+
+		else if (nums[left] <= nums[mid])
+		{
+			if ((nums[left] <= target) && (nums[mid] > target)) right = mid - 1;
+			else left = mid + 1;
+		}
+		else
+		{
+			if ((nums[mid] < target) && (nums[right] >= target)) left = mid + 1;
+			else right = mid - 1;
+		}
+	}
+	return false;
+
+}
+int main_81()
 {
 	vector<int> v{ 1,3,1,1 };
-	int res = search(v, 0);
-	cout << res << endl;
+	bool res = search(v, 3);
+	cout << boolalpha <<  res << endl;
 	system("pause");
 	return 0;
 }
