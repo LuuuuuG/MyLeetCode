23aa721d816603d247ac4b0bb88223214daff958
Gang <776881718@qq.com>
2019-01-28 16:37:05 +0800

    113_Path Sum II.

--- Source/112_Path Sum.cpp
+++ Source/112_Path Sum.cpp
@@ -40,3 +40,11 @@ bool hasPathSum(TreeNode* root, int sum) {
 		return sum == 0;
 	return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
 }
+
+bool hasPathSum2(TreeNode *root, int sum) {
+	if (root == NULL) return false;
+
+	if (root->val == sum && root->left == NULL && root->right == NULL) return true;
+
+	return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+}
